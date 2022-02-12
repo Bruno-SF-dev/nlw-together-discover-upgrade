7ac8e7f7138360c8065ac68880036bbdc1299ae0
Bruno Santos <brunosantosfarias2@gmail.com>
2022-02-11 23:50:10 -0300

    toast para "Código copiado"

--- public/scripts/copyRoomId.js
+++ public/scripts/copyRoomId.js
@@ -1,8 +1,19 @@
 const roomCodeButton = document.getElementById("room-id");
 const roomCode = roomCodeButton.dataset.id;
+const toast = document.getElementById("toast");
+
+const toastComponent = document.createElement("div");
+toastComponent.innerText = "Código copiado!";
+toastComponent.setAttribute("class", "toast");
 
 const getRoomCode = () => {
+  console.log("CHAMOU");
   navigator.clipboard.writeText(roomCode);
+
+  document.body.append(toastComponent);
+  setTimeout(() => {
+    toastComponent.remove();
+  }, 2000);
 };
 
 roomCodeButton.addEventListener("click", getRoomCode);
