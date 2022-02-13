3850570e70717c332e07bfe3a8bf6fd4d05ab0dd
Bruno Santos <brunosantosfarias2@gmail.com>
2022-02-13 03:30:15 -0300

    margin no input

--- src/route.js
+++ src/route.js
@@ -2,12 +2,11 @@ const express = require("express");
 const QuestionController = require("./controllers/QuestionController");
 const RoomController = require("./controllers/RoomController");
 
-// a constante route passa a ter todas as funcionalidades de rotas que o express disponibiliza
 const route = express.Router();
 
 route.get("/", (req, res) =>
   res.render("index", { page: "enter-room", error: req.flash("error") }),
-); // { page: "enter-room" }: passando uma variável "page"
+);
 route.get("/create-pass", (req, res) =>
   res.render("index", { page: "create-pass" }),
 );
